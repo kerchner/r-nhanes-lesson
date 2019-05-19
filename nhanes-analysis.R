7cb9150f9f7fa6ce14624241f557e2fd4893d42d
kerchner <kerchner@gwu.edu>
2019-05-19 18:49:21 -0400

    Merged all 3 data frames

--- nhanes-analysis.R
+++ nhanes-analysis.R
@@ -9,5 +9,7 @@ colnames(df)
 # Let's look at the columns in glu_df
 colnames(glu_df)
 # Let's look at the columns in sleep_df
-colnames(glu_df)
+colnames(sleep_df)
 
+df2 <- merge(df, glu_df)
+df2 <- merge(df2, sleep_df)
