18b09b1af69d194eb3b8467f837d036ee91018df
kerchner <kerchner@gwu.edu>
2019-05-19 18:42:22 -0400

    Added sleep data

--- nhanes-analysis.R
+++ nhanes-analysis.R
@@ -2,3 +2,12 @@ install.packages('SASxport')
 library(SASxport)
 df <- read.xport('data/DEMO_I.XPT')
 glu_df <- read.xport('data/GLU_I.XPT')
+sleep_df <- read.xport('data/SLQ_I.XPT')
+
+# Let's look at the columns in df
+colnames(df)
+# Let's look at the columns in glu_df
+colnames(glu_df)
+# Let's look at the columns in sleep_df
+colnames(glu_df)
+
